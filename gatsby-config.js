91c4ef2d5f738872e668bf3f0ce786000703e03a
rinzin <rinzin.norbu@dcpl.bt>
2021-09-03 15:11:59 +0600

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,16 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
 module.exports = {
-    siteMetadata: {
-        siteUrl: `https://www.yourdomain.tld`,
-    },
-    plugins: [
-
-    ]
-}
+  siteMetadata: {
+    title: 'Dragon Coders',
+    siteUrl: `https://dcpl.bt`,
+    copyright: 'This website is copyright 2021 Dragon Coders Pvt. Ltd.',
+    contact: 'info@dcpl.bt',
+  },
+  plugins: [
+    'gatsby-plugin-gatsby-cloud',
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+  ]
+};
