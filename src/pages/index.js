8b47496a5c9fb9270495945f4973fa405017a261
rinzin <rinzin.norbu@dcpl.bt>
2021-09-03 18:21:00 +0600

    Change image

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,12 +8,12 @@ export default function Home() {
     <Layout>
       <section className={styles.header}>
         <div>
-          <h2>Design</h2>
+          <h2 style={{marginBottom: '15px'}}>Design</h2>
           <h3>Develop & Deploy</h3>
-          <p>UX designer & web developer based in Bhutan.</p>
+          <p style={{marginBottom: '15px'}}>UX designer & web developer based in Bhutan.</p>
           <Link className={styles.btn} to="/projects">My Portfolio Projects</Link>
         </div>
-        <img src="/banner.png" alt="site banner" style={{maxWidth: '100%', height: '450px', marginTop: '60px'}}/>
+        <img src="/banner.png" alt="site banner" style={{maxWidth: '100%'}}/>
       </section>
     </Layout>
   );
