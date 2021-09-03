90732376be9db3b37a448804ddd199362eff22f6
rinzin <rinzin.norbu@dcpl.bt>
2021-09-03 17:07:58 +0600

    Change style import

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,15 +2,15 @@ require('dotenv').config({
   path: `.env.${process.env.NODE_ENV}`,
 });
 module.exports = {
+  plugins: [
+    'gatsby-plugin-gatsby-cloud',
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+  ],
   siteMetadata: {
     title: 'Dragon Coders',
     siteUrl: `https://dcpl.bt`,
     copyright: 'This website is copyright 2021 Dragon Coders Pvt. Ltd.',
     contact: 'info@dcpl.bt',
-  },
-  plugins: [
-    'gatsby-plugin-gatsby-cloud',
-    'gatsby-plugin-image',
-    'gatsby-plugin-sharp',
-  ]
+  }
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,20 @@
-import React from "react"
-import { Link } from 'gatsby'
-import Layout from "../components/Layout"
-import styles from '../styles/home.module.css'
+import React from 'react';
+import * as styles from '../styles/home.module.css';
+import Layout from '../components/Layout';
+import {Link} from 'gatsby';
 
 export default function Home() {
   return (
     <Layout>
-      <section className={styles?.header}>
+      <section className={styles.header}>
         <div>
           <h2>Design</h2>
           <h3>Develop & Deploy</h3>
           <p>UX designer & web developer based in Bhutan.</p>
-          <Link className={styles?.btn} to="/projects">My Portfolio Projects</Link>
+          <Link className={styles.btn} to="/projects">My Portfolio Projects</Link>
         </div>
-        <img src="/banner.jpg" alt="site banner" style={{ minWidth: '100%' }} />
+        <img src="/banner.png" alt="site banner" style={{maxWidth: '100%', height: '450px'}}/>
       </section>
     </Layout>
-  )
+  );
 }
